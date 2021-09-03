5b98461657b699a144d6e2e310a442047dab80f0
quadrii <adisaquadri234@yahoo.com>
2021-09-03 14:58:33 +0100

    practicing forEach and Map

--- challenge/js/script.js
+++ challenge/js/script.js
@@ -6,3 +6,19 @@ let toggleAction =
     toggleIcon.addEventListener('click', function () {
         toggleContent.classList.toggle('hidden')
     })
+
+
+    let movements = [12, 13, 14, 17, 20, 25];
+    let converToUsd = 1.2;
+
+    // will throw undefined
+    let looping = movements.forEach(function (mov) {
+        return mov * converToUsd;
+    })
+    console.log(looping);
+
+    // will work with map
+    let loopingWithMap = movements.map(function (mov) {
+        return mov * converToUsd;
+    })
+    console.log(loopingWithMap);
